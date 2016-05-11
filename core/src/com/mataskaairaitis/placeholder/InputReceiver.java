df2eb7ad5c92673205fbc08734f7f790d70d29b6
jarl <jarl@satellite.localdomain>
2016-05-11 10:45:32 +0200

    Added interface for inputreceivers (playercontrol, menucontrol etc)

---
+++ core/src/com/mataskaairaitis/placeholder/InputReceiver.java
@@ -0,0 +1,17 @@
+package com.mataskaairaitis.placeholder;
+
+/**
+ * Interface for classes that may be held by UserInput,
+ * and receive information from there.
+ * 
+ * (dev notes) May be expanded upon, should certain methods be mandatory.
+ * If a particular implementation is the same for all InputReceivers, the
+ * interface could be made into an abstract class and said implementation be
+ * defined here. 
+ * 
+ * @author jarl
+ * @version 1
+ */
+public interface InputReceiver {
+
+}
