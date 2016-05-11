c8eb3f849390c76e0bdcff57e08bb791000e813c
jarl <jarl@satellite.localdomain>
2016-05-11 10:48:43 +0200

    Changed and added the testclasses to test the new Input classes.

--- core/src/com/mataskaairaitis/placeholder/MyGdxGame.java
+++ core/src/com/mataskaairaitis/placeholder/MyGdxGame.java
@@ -2,7 +2,6 @@ package com.mataskaairaitis.placeholder;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -20,6 +19,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Sprite sprite;
+	UserInput player;
 	
 	@Override
 	public void create () {
@@ -28,25 +28,15 @@ public class MyGdxGame extends ApplicationAdapter {
 		sprite = new Sprite(img);
 		sprite.setPosition(Gdx.graphics.getWidth()/2 - sprite.getWidth()/2, 
 				Gdx.graphics.getHeight()/2 - sprite.getHeight()/2);
+		SpriteControl receiver = new SpriteControl(sprite);
+		player = new UserInput(receiver);
+		Gdx.input.setInputProcessor(player);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-			sprite.translateY(4);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-			sprite.translateY(-4);
-		}
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-        	sprite.translateX(-4);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-        	sprite.translateX(4);
-        }
 		
 		batch.begin();
 		sprite.draw(batch);
---
+++ core/src/com/mataskaairaitis/placeholder/SpriteControl.java
@@ -0,0 +1,79 @@
+package com.mataskaairaitis.placeholder;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Test implementation of InputReceiver on the sprite
+ * testclass MyGdxGame.
+ * 
+ * @author jarl
+ */
+public class SpriteControl implements InputReceiver {
+	
+	private Sprite sprite;
+	private final int sprite_speed = 5; //milliseconds between wait
+	
+	boolean movingUp;
+	boolean movingDown;
+	boolean movingLeft;
+	boolean movingRight;
+	
+	public SpriteControl(Sprite sprite) {
+		this.sprite = sprite;
+	}
+	
+	public void keyPressedUp() {
+		movingUp = true;
+		while (movingUp) {
+			sprite.translateY(1);
+			sleep();
+		}
+	}
+	
+	public void keyReleasedUp() {
+		movingUp = false;
+	}
+	
+	public void keyPressedDown() {
+		movingDown = true;
+		while (movingDown) {
+			sprite.translateY(-1);
+			sleep();
+		}
+	}
+	
+	public void keyReleasedDown() {
+		movingDown = false;
+	}
+	
+	public void keyPressedLeft() {
+		movingLeft = true;
+		while (movingLeft) {
+			sprite.translateX(-1);
+			sleep();
+		}
+	}
+	
+	public void keyReleasedLeft() {
+		movingLeft = false;
+	}
+	
+	public void keyPressedRight() {
+		movingRight = true;
+		while (movingRight) {
+			sprite.translateX(1);
+			sleep();
+		}
+	}
+	
+	public void keyReleasedRight() {
+		movingRight = false;
+	}
+	
+	private void sleep() {
+		try {
+			Thread.sleep(sprite_speed);
+		} catch (Exception e) {}
+	}
+	
+}
