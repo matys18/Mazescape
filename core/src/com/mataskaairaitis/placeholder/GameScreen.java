a3e7bb056a4a2c46515395bb2352c1df5d421a52
Matas Kairaitis <matas.kairaitis@gmail.com>
2016-05-12 10:51:02 +0200

    Split game up into screens

---
+++ core/src/com/mataskaairaitis/placeholder/GameScreen.java
@@ -0,0 +1,104 @@
+package com.mataskaairaitis.placeholder;
+
+import box2dLight.PointLight;
+import box2dLight.RayHandler;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+import java.util.Random;
+
+/**
+ * Created by mataskairaitis on 12/05/16.
+ */
+public class GameScreen implements Screen {
+
+    OrthographicCamera camera;
+    Box2DDebugRenderer renderer;
+    FPSLogger fpsLogger;
+    Body circleBody;
+    PointLight playerLight;
+    World world;
+    RayHandler rayHandler;
+    float width, height;
+
+    public GameScreen(final MyGdxGame game) {
+        width = 1280;
+        height = 720;
+
+        camera = new OrthographicCamera(width, height);
+        camera.position.set(width * 0.5f, height * 0.5f, 0);
+        camera.update();
+
+        world = new World(new Vector2(0, 0), true);
+        renderer = new Box2DDebugRenderer();
+        fpsLogger = new FPSLogger();
+
+        BodyDef circleDef = new BodyDef();
+        circleDef.type = BodyDef.BodyType.DynamicBody;
+        circleDef.position.set(width * 0.5f, height * 0.5f);
+
+        circleBody = world.createBody(circleDef);
+
+        CircleShape circleShape = new CircleShape();
+        circleShape.setRadius(6f);
+
+        FixtureDef circleFixture = new FixtureDef();
+        circleFixture.shape = circleShape;
+        circleFixture.density = 0.4f;
+        circleFixture.friction = 0.2f;
+        circleFixture.restitution = 0.8f;
+
+        circleBody.createFixture(circleFixture);
+
+        // Lighting settings
+        rayHandler = new RayHandler(world);
+        rayHandler.setCombinedMatrix(camera.combined);
+
+        playerLight = new PointLight(rayHandler, 50000, Color.ORANGE, 100, width / 2, height / 2);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1); // Sets background to black
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clears the screen
+
+        // Render the world
+        renderer.render(world, camera.combined);
+        world.step(1/60f, 6, 2);
+
+        // Log the fps
+        fpsLogger.log();
+
+        // Box2dlight stuff
+        playerLight.setDistance(new Random().nextInt(200));
+        rayHandler.updateAndRender();
+    }
+
+
+    @Override
+    public void resize(int width, int height) {}
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void dispose() {
+        world.dispose();
+    }
+
+}
--- core/src/com/mataskaairaitis/placeholder/MyGdxGame.java
+++ core/src/com/mataskaairaitis/placeholder/MyGdxGame.java
@@ -3,6 +3,7 @@ package com.mataskaairaitis.placeholder;
 import box2dLight.PointLight;
 import box2dLight.RayHandler;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -29,88 +30,23 @@ import java.util.Random;
  * All input is now handled by UserInput class, which also uses
  * reflection to direct input to methods instead of if-statements.
  */
-public class MyGdxGame extends ApplicationAdapter {
-	OrthographicCamera camera;
-	Box2DDebugRenderer renderer;
-	FPSLogger fpsLogger;
-	Body circleBody;
-	PointLight kek;
+public class MyGdxGame extends Game {
 
-	float width, height;
+	GameScreen gameScreen;
 
-	World world;
-	RayHandler rayHandler;
-
-	Circle player;
-	
 	@Override
 	public void create () {
-		width = 1280;
-		height = 720;
-
-		camera = new OrthographicCamera(width, height);
-		camera.position.set(width * 0.5f, height * 0.5f, 0);
-		camera.update();
-
-		world = new World(new Vector2(0, 0), true);
-		renderer = new Box2DDebugRenderer();
-		fpsLogger = new FPSLogger();
-
-		BodyDef circleDef = new BodyDef();
-		circleDef.type = BodyDef.BodyType.DynamicBody;
-		circleDef.position.set(width * 0.5f, height * 0.5f);
-
-		circleBody = world.createBody(circleDef);
-
-		CircleShape circleShape = new CircleShape();
-		circleShape.setRadius(6f);
-
-		FixtureDef circleFixture = new FixtureDef();
-		circleFixture.shape = circleShape;
-		circleFixture.density = 0.4f;
-		circleFixture.friction = 0.2f;
-		circleFixture.restitution = 0.8f;
-
-		circleBody.createFixture(circleFixture);
-
-		// Lighting settings
-		rayHandler = new RayHandler(world);
-		rayHandler.setCombinedMatrix(camera.combined);
-
-		kek = new PointLight(rayHandler, 50000, Color.ORANGE, 100, width / 2 - 50, height / 2 + 15);
+		gameScreen  = new GameScreen(this);
+		this.setScreen(gameScreen);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1); // Sets background to black
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clears the screen
-
-		renderer.render(world, camera.combined);
-		world.step(1/60f, 6, 2);
-
-		fpsLogger.log();
-
-		kek.setDistance(new Random().nextInt(200));
-		rayHandler.updateAndRender();
+		super.render();
 	}
 
 	@Override
 	public void dispose() {
-		world.dispose();
-	}
-
-	@Override
-	public void pause() {
-
-	}
-
-	@Override
-	public void resume() {
-
-	}
-
-	@Override
-	public void resize(int width, int height) {
 
 	}
 }
