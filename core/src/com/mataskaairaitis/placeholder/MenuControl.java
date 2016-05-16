4981ca61b14b073483f7d3e843bfe021a73cb161
jarl <jarl@satellite.localdomain>
2016-05-16 19:23:08 +0200

    Added deactivation of labels - greyed-out can't be highlighted.

--- core/src/com/mataskaairaitis/placeholder/MenuControl.java
+++ core/src/com/mataskaairaitis/placeholder/MenuControl.java
@@ -1,6 +1,7 @@
 package com.mataskaairaitis.placeholder;
 
-import com.badlogic.gdx.Gdx;;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 
 /**
  * Sample Menu control class, for testing purposes.
@@ -9,17 +10,33 @@ import com.badlogic.gdx.Gdx;;
 public class MenuControl implements InputReceiver {
 	
 	MenuScreen screen;
+	int highlight = 0;
 	
 	public MenuControl(MenuScreen screen) {
 		this.screen = screen;
 	}
 	
 	public void keyPressedUp() {
-		screen.highlightedItem = (screen.highlightedItem + screen.menuItems.length - 1) % screen.menuItems.length;
+		switchColor(highlight, moveUp(1));
 	}
 	
 	public void keyPressedDown() {
-		screen.highlightedItem = (screen.highlightedItem + 1) % screen.menuItems.length;
+		switchColor(highlight, moveDown(1));
+	}
+	
+	private int moveUp(int steps) {
+		int length = screen.labels.length;
+		int next = (highlight + length - steps) % length;
+		if(screen.labels[next].getColor().equals(Color.LIGHT_GRAY))
+			return moveUp(steps + 1);
+		return next;
+	}
+	
+	private int moveDown(int steps) {
+		int next = (highlight + steps) % screen.labels.length;
+		if(screen.labels[next].getColor().equals(Color.LIGHT_GRAY))
+			return moveDown(steps + 1);
+		return next;
 	}
 	
 	public void keyPressedSpace() {
@@ -29,15 +46,25 @@ public class MenuControl implements InputReceiver {
 	public void keyPressedEnter() {
 		activateKey();
 	}
+	
+	public void switchColor(int prev, int next) {
+		screen.labels[prev].setColor(Color.WHITE);
+		highlight = next;
+		screen.labels[next].setColor(Color.ORANGE);
+	}
 
 	private void activateKey() {
-		switch (screen.highlightedItem) {
+		switch (highlight) {
 		case 0:
+			screen.game.gameScreen = new GameScreen(screen.game);
 			screen.game.setScreen(screen.game.gameScreen);
 			break;
 		case 1:
+			screen.game.setScreen(screen.game.gameScreen);
 			break;
 		case 2:
+			break;
+		case 3:
 			Gdx.app.exit();
 		}
 	}
