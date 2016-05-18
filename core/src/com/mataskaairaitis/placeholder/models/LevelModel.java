d27d7fc4de1a2e273c79aeec0e617da58221d16a
jarl <jarl@satellite.localdomain>
2016-05-18 12:21:12 +0200

    Further improved on wallbuilding methods.

--- core/src/com/mataskaairaitis/placeholder/models/LevelModel.java
+++ core/src/com/mataskaairaitis/placeholder/models/LevelModel.java
@@ -12,12 +12,14 @@ public class LevelModel {
     World world;
     float width;
     float height;
+    protected float thickness;
     private Array<WallModel> obstacles;
 
-    public LevelModel(World world, float width, float height) {
+    public LevelModel(World world, float width, float height, float thickness) {
         this.world = world;
         this.width = width;
         this.height = height;
+        this.thickness = thickness;
 
         obstacles = new Array<WallModel>();
 
@@ -26,11 +28,26 @@ public class LevelModel {
         obstacles.add(new WallModel(width - 10f, 0f, 10f, height, world));
         obstacles.add(new WallModel(0f, 0f, width, 10f, world));
         obstacles.add(new WallModel(0f, height - 10f, width, 10f, world));
-//        makeWall(width/2, height/3, 100, 10);
     }
     
-    public void makeWall(float x, float y, float width, float height) {
-    	obstacles.add(new WallModel(x, y, width, height, world));
+    /**
+     * Make a horizontal wall somewhere on the Level.
+     * @param x1 left x coordinate
+     * @param x2 right x coordinate
+     * @param y
+     */
+    public void horiWall(float x1, float x2, float y) {
+    	obstacles.add(new WallModel((x1 + x2)/2, y, (x2 - x1)/2, thickness, world));
+    }
+    
+    /**
+     * Make a vertical wall somewhere on the level.
+     * @param x
+     * @param y1 lower y coordinate
+     * @param y2 upper y coordinate
+     */
+    public void vertWall(float x, float y1, float y2) {
+    	obstacles.add(new WallModel(x, (y1 + y2)/2, thickness, (y2-y1)/2, world));
     }
 
     public Array<WallModel> getObstacles() {
